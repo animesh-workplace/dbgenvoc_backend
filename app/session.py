8b30c3e198a1d15e372b21af828698b264ea65bc
Animesh <animesh.workplace@gmail.com>
2025-10-16 22:15:15 +0530

    updated file - app/session.py

--- app/session.py
+++ app/session.py
@@ -1,8 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
+from agno.models.aws import AwsBedrock
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# For SQLite (converted)
+load_dotenv()
 SQLALCHEMY_DATABASE_URL = "sqlite:///database/database.sqlite3"
 
 engine = create_engine(
@@ -10,6 +13,13 @@ engine = create_engine(
     connect_args={"check_same_thread": False}
     if "sqlite" in SQLALCHEMY_DATABASE_URL
     else {},
+)
+
+ai_engine = AwsBedrock(
+    id=os.getenv("AMAZON_MODEL_ID"),
+    aws_region=os.getenv("AWS_REGION"),
+    aws_access_key_id=os.getenv("AMAZON_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AMAZON_SECRET_ACCESS_KEY"),
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
