3b82dac37e8757115811dc256fdc4ce059ed0640
Animesh <animesh.workplace@gmail.com>
2025-10-26 12:51:34 +0530

    updated file - app/api/oncoplot.py

--- app/api/oncoplot.py
+++ app/api/oncoplot.py
@@ -1,4 +1,4 @@
-from sqlalchemy import func
+from sqlalchemy import func, case
 import fireducks.pandas as pandas
 from fastapi import HTTPException
 from app.core import row_to_dict, get_model_class
@@ -9,7 +9,7 @@ async def oncoplot_search(request: OncoplotRequest, table_name, db):
     """Search function that returns data in ECharts heatmap format"""
     try:
         model_class = get_model_class(table_name)
-        genes = getattr(request, "genes", None)
+        genes = list(set(getattr(request, "genes", None)))
 
         if not genes:
             return OncoplotResponse(yAxis=[], xAxis=[], heatmap=[])
@@ -17,6 +17,7 @@ async def oncoplot_search(request: OncoplotRequest, table_name, db):
         query = db.query(model_class)
         query = query.filter(model_class.gene.in_(genes))
         results = query.all()
+
         gene_zero_counts = dict(
             db.query(model_class.gene, func.count(model_class.gene))
             .filter(model_class.gene.in_(genes), model_class.evalue == 0)
@@ -30,6 +31,37 @@ async def oncoplot_search(request: OncoplotRequest, table_name, db):
         data_list = [row_to_dict(row) for row in results]
         df = pandas.DataFrame(data_list)
         df["value"] = (df["evalue"] != 0).astype(int)
+        df["annotation"] = df["annotation"].replace("", "None")
+
+        # Section for the number of sample data
+        df_sample_bar_plot = df.pivot_table(
+            fill_value=0,
+            index="gene",
+            aggfunc="sum",
+            values="value",
+            columns="annotation",
+        )
+        df_sample_bar_plot = df_sample_bar_plot.loc[sorted_genes].drop(columns="None")
+        sample_bar_plot = [
+            {
+                "name": col,
+                "type": "bar",
+                "stack": "total_sample",
+                "data": df_sample_bar_plot[col].tolist(),
+            }
+            for col in df_sample_bar_plot.columns
+        ]
+
+        # Section for TMB data calculation
+        df_tmb_plot = df.pivot_table(
+            fill_value=0,
+            aggfunc="sum",
+            values="value",
+            index="sample_id",
+            columns="annotation",
+        )
+
+        # Section for the oncoplot data
         df = df.pivot_table(
             fill_value=0,
             aggfunc="max",
@@ -39,6 +71,17 @@ async def oncoplot_search(request: OncoplotRequest, table_name, db):
         )
         df = df.sort_values(by=sorted_genes[::-1], ascending=False)
         samples = df.index.tolist()
+
+        df_tmb_plot = df_tmb_plot.loc[samples].drop(columns="None")
+        tmb_bar_plot = [
+            {
+                "name": col,
+                "type": "bar",
+                "stack": "total_tmb",
+                "data": df_tmb_plot[col].tolist(),
+            }
+            for col in df_tmb_plot.columns
+        ]
 
         # Create mapping from sample/gene to index
         sample_to_idx = {sample: idx for idx, sample in enumerate(samples)}
@@ -54,7 +97,13 @@ async def oncoplot_search(request: OncoplotRequest, table_name, db):
 
             heatmap_data.append([sample_idx, gene_idx, evalue])
 
-        return OncoplotResponse(yAxis=sorted_genes, xAxis=samples, heatmap=heatmap_data)
+        return OncoplotResponse(
+            xAxis=samples,
+            yAxis=sorted_genes,
+            heatmap=heatmap_data,
+            tmb_bar_plot=tmb_bar_plot,
+            sample_bar_plot=sample_bar_plot,
+        )
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Search failed: {str(e)}")
