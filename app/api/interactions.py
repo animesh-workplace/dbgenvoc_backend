ef71bd6b1fa6d30ef1cad40b4b9010f16c092728
Animesh <animesh.workplace@gmail.com>
2025-10-27 21:16:49 +0530

    updated file - app/api/interactions.py

--- app/api/interactions.py
+++ app/api/interactions.py
@@ -1,7 +1,8 @@
+import traceback
 import fireducks.pandas as pandas
 from fastapi import HTTPException
 from app.core import row_to_dict, get_model_class
-from app.schema import OncoplotRequest, OncoplotResponse
+from app.schema import OncoplotRequest, InteractionResponse
 
 
 async def interaction_search(request: OncoplotRequest, table_name, db):
@@ -11,7 +12,7 @@ async def interaction_search(request: OncoplotRequest, table_name, db):
         genes = getattr(request, "genes", None)
 
         if not genes:
-            return OncoplotResponse(yAxis=[], xAxis=[], heatmap=[])
+            return InteractionResponse(yAxis=[], xAxis=[], heatmap=[])
 
         # Get all unique combinations of the input genes
         query = db.query(model_class)
@@ -23,10 +24,12 @@ async def interaction_search(request: OncoplotRequest, table_name, db):
         # Convert to DataFrame for easier manipulation
         data_list = [row_to_dict(row) for row in results]
         df = pandas.DataFrame(data_list)
+
         df_symmetric = df[["gene1", "gene2", "neg_log10_pval"]].copy()
         df_reverse = df[["gene2", "gene1", "neg_log10_pval"]].copy()
         df_reverse.columns = ["gene1", "gene2", "neg_log10_pval"]
         df_combined = pandas.concat([df_symmetric, df_reverse], ignore_index=True)
+        print(df_combined)
 
         # Create pivot table
         df_pivot = df_combined.pivot_table(
@@ -36,7 +39,7 @@ async def interaction_search(request: OncoplotRequest, table_name, db):
             columns="gene2",
             values="neg_log10_pval",
         )
-        print(df[["gene1", "gene2", "neg_log10_pval"]])
+        print(df_pivot)
         # Build heatmap data for upper triangle only
         heatmap_data = []
         for i, gene_y in enumerate(sorted_genes):  # y-axis (rows)
@@ -49,11 +52,12 @@ async def interaction_search(request: OncoplotRequest, table_name, db):
                         and original_row["odds_ratio"].iloc[0] < 1
                     ):
                         value = -value
-                    heatmap_data.append([i, j, value])
+                    heatmap_data.append([gene_y, gene_x, value])
 
-        return OncoplotResponse(
+        return InteractionResponse(
             yAxis=sorted_genes, xAxis=sorted_genes, heatmap=heatmap_data
         )
 
     except Exception as e:
+        traceback.print_exc()
         raise HTTPException(status_code=500, detail=f"Search failed: {str(e)}")
