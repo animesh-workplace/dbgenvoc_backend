b70dc97826f39d0c4005cba1d16aadf07000ffc3
Animesh <animesh.workplace@gmail.com>
2025-10-28 12:32:49 +0530

    updated file - app/abstract.py

--- app/abstract.py
+++ app/abstract.py
@@ -1,6 +1,6 @@
 from app.session import Base
 from sqlalchemy.orm import relationship, declared_attr
-from sqlalchemy import String, Column, Integer, Float, ForeignKey, Table, Text
+from sqlalchemy import String, Column, Integer, Float, ForeignKey, Table, Text, Index
 
 # Association table
 pathway_gene_association = Table(
@@ -24,8 +24,8 @@ class Genelist(Base):
 class Pathway(Base):
     __tablename__ = "pathway"
 
-    id = Column(String(12), primary_key=True)
     pathway_name = Column(String(300))
+    id = Column(String(12), primary_key=True)
     genes = relationship(
         "Genelist", secondary=pathway_gene_association, back_populates="pathways"
     )
@@ -40,10 +40,10 @@ class PatientBarcode(Base):
 class Uniprot(Base):
     __tablename__ = "uniprot_structure"
 
+    structure = Column(Text)
+    accession_id = Column(String(20))
     id = Column(Integer, primary_key=True, autoincrement=True)
     gene = Column(String(20), ForeignKey("genelist.gene"), index=True)
-    accession_id = Column(String(20))
-    structure = Column(Text)
 
     @declared_attr
     def genelist(cls):
@@ -53,32 +53,32 @@ class Uniprot(Base):
 class BaseVariantModel(Base):
     __abstract__ = True
 
-    variant_id = Column(Integer, primary_key=True, autoincrement=True)
-    gene = Column(String(20), ForeignKey("genelist.gene"), index=True)
-    entrez_gene_id = Column(Integer)
+    end = Column(String(30))
+    chrom = Column(String(6))
+    start = Column(String(30))
+    disease = Column(String(100))
     ncbi_build = Column(String(6))
-    chrom = Column(String(6), index=True)
-    start = Column(String(30), index=True)
-    end = Column(String(30), index=True)
-    variant_class = Column(String(100))
-    variant_type = Column(String(10))
-    ref_allele = Column(String(50))
-    tumor_seq_allele2 = Column(String(50))
     dbsnp_rs = Column(String(200))
+    ref_allele = Column(String(50))
+    reference = Column(String(200))
+    entrez_gene_id = Column(Integer)
+    cDNA_change = Column(String(30))
+    variant_type = Column(String(10))
+    codon_change = Column(String(30))
+    genome_change = Column(String(75))
+    variant_class = Column(String(100))
+    protein_change = Column(String(20))
+    reference_url = Column(String(900))
+    transcript_exon = Column(String(10))
+    transcript_strand = Column(String(2))
+    tumor_seq_allele2 = Column(String(50))
+    transcript_position = Column(String(20))
+    annotation_transcript = Column(String(20))
     tumor_sample_barcode = Column(
         String(20), ForeignKey("patient_barcode.id"), index=True
     )
-    genome_change = Column(String(75))
-    annotation_transcript = Column(String(20))
-    transcript_strand = Column(String(2))
-    transcript_exon = Column(String(10))
-    transcript_position = Column(String(20))
-    cDNA_change = Column(String(30))
-    codon_change = Column(String(30))
-    protein_change = Column(String(20))
-    disease = Column(String(100))
-    reference_url = Column(String(900))
-    reference = Column(String(200))
+    variant_id = Column(Integer, primary_key=True, autoincrement=True)
+    gene = Column(String(20), ForeignKey("genelist.gene"), index=True)
 
     @declared_attr
     def genelist(cls):
@@ -88,17 +88,27 @@ class BaseVariantModel(Base):
     def barcode(cls):
         return relationship("PatientBarcode", lazy="joined")
 
+    @declared_attr
+    def __table_args__(cls):
+        return (
+            Index(f"index_{cls.__tablename__}_gene_chrom", "gene", "chrom"),
+            Index(f"index_{cls.__tablename__}_chrom_start", "chrom", "start"),
+            Index(
+                f"index_{cls.__tablename__}_chrom_start_end", "chrom", "start", "end"
+            ),
+        )
+
 
 class BaseOncoplotModel(Base):
     __abstract__ = True
 
+    code = Column(Integer)
+    annotation = Column(String(255))
     id = Column(Integer, primary_key=True, autoincrement=True)
-    gene = Column(String(20), ForeignKey("genelist.gene"), index=True)
     tumor_sample_barcode = Column(
         String(20), ForeignKey("patient_barcode.id"), index=True
     )
-    code = Column(Integer)
-    annotation = Column(String(255))
+    gene = Column(String(20), ForeignKey("genelist.gene"), index=True)
 
     @declared_attr
     def genelist(cls):
@@ -110,15 +120,14 @@ class BaseOncoplotModel(Base):
 
 
 class BaseGeneInteractionModel(Base):
-    # Requires index when creating Model from this
     __abstract__ = True
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    gene1 = Column(String(20), ForeignKey("genelist.gene"), index=True)
-    gene2 = Column(String(20), ForeignKey("genelist.gene"), index=True)
     p_value = Column(Float)
     odds_ratio = Column(Float)
     neg_log10_pval = Column(Float)
+    gene1 = Column(String(20), ForeignKey("genelist.gene"))
+    gene2 = Column(String(20), ForeignKey("genelist.gene"))
+    id = Column(Integer, primary_key=True, autoincrement=True)
 
     @declared_attr
     def gene1_obj(cls):
@@ -128,16 +137,20 @@ class BaseGeneInteractionModel(Base):
     def gene2_obj(cls):
         return relationship("Genelist", foreign_keys=[cls.gene2], lazy="joined")
 
+    @declared_attr
+    def __table_args__(cls):
+        return (Index(f"index_{cls.__tablename__}_gene1_gene2", "gene1", "gene2"),)
+
 
 class BaseSampleTMBModel(Base):
     __abstract__ = True
 
+    count = Column(Integer)
+    variant_class = Column(String(100))
     id = Column(Integer, primary_key=True, autoincrement=True)
     tumor_sample_barcode = Column(
         String(20), ForeignKey("patient_barcode.id"), index=True
     )
-    variant_class = Column(String(100))
-    count = Column(Integer)
 
     @declared_attr
     def barcode(cls):
@@ -147,12 +160,12 @@ class BaseSampleTMBModel(Base):
 class BaseSampleTiTvModel(Base):
     __abstract__ = True
 
+    count = Column(Integer)
+    snv_class = Column(String(10))
     id = Column(Integer, primary_key=True, autoincrement=True)
     tumor_sample_barcode = Column(
         String(20), ForeignKey("patient_barcode.id"), index=True
     )
-    snv_class = Column(String(10))
-    count = Column(Integer)
 
     @declared_attr
     def barcode(cls):
