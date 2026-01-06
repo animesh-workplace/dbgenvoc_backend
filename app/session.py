1bf21646ed71c529637cf4e763df2ca5ac40f602
Animesh <animesh.workplace@gmail.com>
2026-01-07 00:33:10 +0530

    updated file - app/session.py

--- app/session.py
+++ app/session.py
@@ -2,11 +2,16 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from agno.models.aws import AwsBedrock
+from langtrace_python_sdk import langtrace
 from sqlalchemy.orm import registry, sessionmaker
 # from sqlalchemy.ext.declarative import declarative_base
 
 load_dotenv()
 SQLALCHEMY_DATABASE_URL = "sqlite:///database/database.sqlite3"
+
+langtrace.init(
+    api_key="e46c7e7a3da2478c4864f3c46beab8a00416a4fbfcfd175390e159e731b97a91"
+)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
@@ -15,8 +20,15 @@ engine = create_engine(
     else {},
 )
 
-ai_engine = AwsBedrock(
-    id=os.getenv("AMAZON_MODEL_ID"),
+ai_engine_pro = AwsBedrock(
+    id=os.getenv("AMAZON_MODEL_ID_PRO"),
+    aws_region=os.getenv("AWS_REGION"),
+    aws_access_key_id=os.getenv("AMAZON_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AMAZON_SECRET_ACCESS_KEY"),
+)
+
+ai_engine_lite = AwsBedrock(
+    id=os.getenv("AMAZON_MODEL_ID_LITE"),
     aws_region=os.getenv("AWS_REGION"),
     aws_access_key_id=os.getenv("AMAZON_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AMAZON_SECRET_ACCESS_KEY"),
